a58bd20cdd16fe5b98ca8e639c962b795975a47e
Sam Brauer <sbrauer@rentpath.com>
2019-08-21 14:08:22 -0400

    Initial commit
    As of
    https://github.com/fulcrologic/fulcro-developer-guide/blob/master/DevelopersGuide.adoc#composing

---
+++ src/main/app/client.cljs
@@ -0,0 +1,14 @@
+(ns app.client
+  (:require
+    [app.application :refer [app]]
+    [app.ui :as ui]
+    [com.fulcrologic.fulcro.application :as app]))
+
+(defn ^:export init []
+  (app/mount! app ui/Root "app")
+  (js/console.log "Loaded"))
+
+(defn ^:export refresh []
+  ;; re-mounting will cause forced UI refresh
+  (app/mount! app ui/Root "app")
+  (js/console.log "Hot reload"))
