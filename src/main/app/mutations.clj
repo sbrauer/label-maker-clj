956611bb26ff5ad7d87b868b9a3b22584719d0ee
Sam Brauer <sbrauer@rentpath.com>
2019-08-23 15:30:18 -0400

    WIP Text labeling component

--- src/main/app/mutations.clj
+++ src/main/app/mutations.clj
@@ -1,14 +1,29 @@
 (ns app.mutations
   (:require
-    [app.resolvers :refer [list-table]]
+    [app.resolvers :as res]
     [com.wsscode.pathom.connect :as pc]
     [taoensso.timbre :as log]))
 
-(pc/defmutation delete-person [env {list-id   :list/id
-                                    person-id :person/id}]
+(pc/defmutation delete-phrase
+  [env {phrase-id :phrase/id text-id :text/id}]
   ;; optional, this is how you override what symbol it responds to.  Defaults to current ns.
-  {::pc/sym `delete-person}
-  (log/info "Deleting person" person-id "from list" list-id)
-  (swap! list-table update list-id update :list/people (fn [old-list] (filterv #(not= person-id %) old-list))))
+  {::pc/sym `delete-phrase}
+  (log/info "Deleting phrase" phrase-id "from text" text-id)
+  ;; Remove the phrase from the text
+  (swap! res/text-table update text-id update :text/phrases (fn [old-list] (filterv #(not= phrase-id %) old-list)))
+  ;; Remove the phrase
+  (swap! res/phrase-table dissoc phrase-id))
 
-(def mutations [delete-person])
+(pc/defmutation add-phrase
+  [env {text-id :text/id pos :phrase/pos label-id :label/id :keys [tempid]}]
+  ;; optional, this is how you override what symbol it responds to.  Defaults to current ns.
+  {::pc/sym `add-phrase}
+  (log/info "Adding phrase to text" text-id "at pos" pos "for label" label-id)
+  (let [phrase-id (res/next-seq! res/phrase-seq)]
+    ;; Add the new phrase
+    (swap! res/phrase-table assoc phrase-id {:phrase/id phrase-id :phrase/pos pos :phrase/label label-id})
+    ;; Add the new phrase to the text
+    (swap! res/text-table update text-id update :text/phrases conj phrase-id)
+    {:tempids {tempid phrase-id}}))
+
+(def mutations [delete-phrase add-phrase])
