3bb67bbe751b85f20c9b205500275df67c4bc0a4
Sam Brauer <sbrauer@rentpath.com>
2019-08-26 17:16:41 -0400

    Style labelled query parts
    [Card](https://rentpath.atlassian.net/browse/query-component-view)

--- src/main/app/ui.cljs
+++ src/main/app/ui.cljs
@@ -4,6 +4,30 @@
             [cljs.pprint :as pp]
             [clojure.string :as str]))
 
+(defn analyze
+  "Given an input string and a sequence of labeled parts (each with a label and a 2-tuple of position indices), return an ordered sequence of all parts (both labeled and unlabeled) of the string."
+  [input parts]
+  (let [labeled-positions (transduce (map (fn [{:qp/keys [pos label]}]
+                                            (let [[start end] pos
+                                                  ;;{:label/keys [id]} label
+                                                  substr (subs input start end)]
+                                              {:str substr :label label :pos pos})))
+                                     conj
+                                     []
+                                     parts)
+        labeled-idxs (into #{} (mapcat (partial apply range)) (map :qp/pos parts))
+        unlabeled-positions (->> (map-indexed
+                                  vector
+                                  (reduce (fn [acc idx]
+                                            (assoc acc idx nil))
+                                          (vec input)
+                                          labeled-idxs))
+                                 (partition-by (comp some? second))
+                                 (filter (comp second first))
+                                 (map (fn [indexed-chars]
+                                        {:pos [(first (first indexed-chars)) (inc (first (last indexed-chars)))] :str (apply str (map second indexed-chars))})))]
+    (sort-by :pos (concat labeled-positions unlabeled-positions))))
+
 (defsc Label [this {:label/keys [id color]}]
   {:query [:label/id :label/color]
    :ident :label/id}
@@ -14,49 +38,29 @@
 
 (def ui-label (comp/factory Label {:keyfn :label/id}))
 
-(defsc QP [this {:qp/keys [id pos label]}]
+(defsc QP [this {:qp/keys [id pos label] :as FIXME}]
   {:query [:qp/id :qp/pos {:qp/label (comp/get-query Label)}]
-   :ident :qp/id})
+   :ident :qp/id}
+  ;;(dom/span (str FIXME))
+  )
 
-#_(def ui-qp (com/factory QP))
+;;(def ui-qp (comp/factory QP {:keyfn (comp first :qp/pos)}))
 
-(defn analyze
-  [input parts]
-  (prn {:input input :parts parts}) ; FIXME: del
-  (let [schema (into (sorted-map)
-                     (map (juxt :qp/pos :qp/label))
-                     parts)
-        _ (prn [:schema schema])
-        span  (into (sorted-set)
-                    (mapcat (partial apply range)) (keys schema))
-        output (str/trim
-                (apply str
-                       (keep-indexed (fn [i c]
-                                       (when-not (contains? span i) c))
-                                     input)))]
-    {:input input
-     :output output
-     :labels (transduce (map (fn [[[start end :as pos]
-                                   {:label/keys [id color]} :as arg]]
-                               (prn :arg arg)
-                               (let [substr (subs input start end)]
-                                 {:str substr :label id :pos pos})))
-                        conj
-                        []
-                        schema)}))
+(defn present-part
+  [{:keys [pos str label] :as part}]
+  (let [color (:label/color label)]
+    ;; FIXME: remove unneeded attrs
+    (dom/span {:data-start (first pos) :data-end (last pos) :data-label-id (:label/id label) :style {:backgroundColor color}} str)))
 
 (defsc Q [this {:q/keys [id input parts]}]
   {:query [:q/id :q/input {:q/parts (comp/get-query QP)}]
    :ident :q/id}
-  (let [{:keys [output labels]} (analyze input parts)]
-    (prn [:labels labels])
-   (dom/div
-    (dom/h4 (str "QueryID: " id))
-    (dom/h4 (str "Input: " input))
-    (dom/h4 (str "Output: " output))
-    (dom/pre (with-out-str (pp/print-table labels)))
-    (dom/h4 "Editor (WIP):")
-    (dom/div :.query-editor input))))
+  (dom/div
+   (dom/h4 (str "QueryID: " id))
+   (dom/h4 (str "Input: " input))
+   (dom/h4 "Editor (WIP):")
+   ;;(dom/pre :.query-editor (with-out-str (pp/print-table (analyze input parts))))
+   (dom/div :.query-editor (map present-part (analyze input parts)))))
 
 (def ui-q (comp/factory Q {:keyfn :q/id}))
 
